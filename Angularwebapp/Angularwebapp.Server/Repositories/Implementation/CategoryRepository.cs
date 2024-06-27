0dc1d1192f130c8d25148258f7dccc87797eaddd
Roshan <mcaroshan2@gmail.com>
2024-06-28 05:10:27 +0530

    hello

---
+++ Angularwebapp/Angularwebapp.Server/Repositories/Implementation/CategoryRepository.cs
@@ -0,0 +1,24 @@
+﻿using Angularwebapp.Server.Data;
+using Angularwebapp.Server.Model.Damain;
+using Angularwebapp.Server.Repositories.Interface;
+using System.Threading.Tasks;
+
+namespace Angularwebapp.Server.Repositories.Implementation
+{
+    public class CategoryRepository : IcategoryReposetory
+
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public CategoryRepository(ApplicationDbContext dbContext) {
+            this.dbContext = dbContext;
+        }
+        public  async Task<Category> CreateAsync(Category category)
+        {
+            await dbContext.Categories.AddAsync(category);
+            await dbContext.SaveChangesAsync();
+
+            return category;
+        }
+    }
+}
--- Angularwebapp/angularwebapp.client/src/app/app.module.ts
+++ Angularwebapp/angularwebapp.client/src/app/app.module.ts
@@ -5,15 +5,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './Core/Component/navbar/navbar.component';
+import { CategoryListComponent } from './Features/Category/category-list/category-list.component';
+import { AddCategoryComponent } from './Features/Category/add-category/add-category.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    CategoryListComponent,
+    AddCategoryComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
